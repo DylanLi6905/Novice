cc51dc0274322e9f073e09f4aff2d673f715f3db
DylanLi6905 <dli69@buffalo.edu>
2026-03-27 21:49:04 -0400

    backend folder, init package.json and route backend to front

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 8001
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello world' })
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
--- frontend/src/routes/homepage.tsx
+++ frontend/src/routes/homepage.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 import Navbar from '../components/Navbar'
 
 export default function Homepage() {
+  const [message, setMessage] = useState('Loading...')
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+  }, [])
+
   return (
     <div>
       <h1>Homepage</h1>
-      <Navbar/>
+      <p>{message}</p>
+      <Navbar />
       <Link to="/dashboard">Go to Dashboard</Link>
     </div>
   )
