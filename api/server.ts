48703efa9ceb1dac824d2c572e1f4825b1a5f017
ChuTown <mchu341@gmail.com>
2026-04-16 00:37:59 -0400

    add CORS

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express from 'express'
 import cookieParser from "cookie-parser";
 import { authRouter } from "./routes/auth/accounts.js";
@@ -8,6 +9,10 @@ import { authRouter } from "./routes/auth/accounts.js";
 const app = express()
 const port = 8001
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 app.use(cookieParser())
 app.use("/api/auth", authRouter);
 
